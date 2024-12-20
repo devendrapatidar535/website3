68ece06c03c19c6daab093e8dd6ff074906c1bfb
Devendra Patidar <devpatidar593@gmail.com>
2024-12-20 12:54:46 +0530

    index modified

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
