00972794323edb3ab6006903ca3de10ed61add2e
Devendra Patidar <devpatidar593@gmail.com>
2024-12-20 15:29:07 +0530

    add singup validation page with zod validation

--- middlewares/validateBody.middleware.js
+++ middlewares/validateBody.middleware.js
@@ -6,12 +6,30 @@
     try {
       req.body = schema.parse(req.body);
       next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        res.status(400).json({ error: error.errors });
-      }
+    // } catch (error) {
+    //   if (error instanceof ZodError) {
+    //     //res.status(400).json({ error: error.errors });
+
+    //   }
+    // }
+    
+      }  catch (error) {
+        if (error.errors) {
+            // Render the error page with validation errors
+            return res.render('error', { 
+                errors: error.errors 
+            });
+        }
+        
+        // Handle other types of errors
+        return res.render('error', { 
+            errors: [{ message: 'An unexpected error occurred. Please try again.' }] 
+        });
     }
-  } 
+
+
+
+  }  
 
   module.exports = validateRequestBody; 
   
